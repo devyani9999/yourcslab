595fddf0e26adc7d7dfaa1788c333f50fd487a3a
Devyani Rohilla <devyanirohilla@Devyanis-MacBook-Pro.local>
2024-01-22 05:08:53 +0530

    removed debugger

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,18 +8,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
   def google_oauth2
-    debugger
     user = User.from_omniauth(auth)
     if user.blank?
       user = User.create_from_omniauth(auth)
     end
-    debugger
     if user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
-      debugger
       flash[:alert] =
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
@@ -31,7 +28,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    debugger
     user_dashboard_path(resource_or_scope.id) || root_path
   end
 
